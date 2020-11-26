d28f622dcd39e8906f46def172c6e4e35c2ce7c0
Blanchette Alexandre <alexandre.blanchette.ext@retraitequebec.gouv.qc.ca>
2020-11-26 15:01:18 -0500

    Remplissage liste organisations #2

--- app/main/views/platform_admin.py
+++ app/main/views/platform_admin.py
@@ -15,9 +15,10 @@ from app import (
     notification_api_client,
     platform_stats_api_client,
     service_api_client,
+    organisations_client,
 )
 
-from app.models.organisation import Organisations
+
 
 from app.extensions import antivirus_client, redis_client
 from app.main import main
@@ -371,11 +372,16 @@ def usage_for_all_services_by_organisation():
             flash('No results for dates')
         flash('On a réussi à peser sur le bouton  ' + str(organisation_id) + ' ' + str(start_date) + ' ' + str(end_date))
 
-    return render_template(
-        'views/platform-admin/usage_for_all_services_by_organisation.html',
-        form=form,
-        organisations=Organisations()
-    )
+    # METTRE ÇA EN PLACE
+    # api_key_list = api_key_api_client.get_api_keys_ranked_by_notifications_created(n_days_back)
+    # return render_template(
+    #     'views/platform-admin/api_keys_ranked.html',
+    #     api_key_list=api_key_list
+    # )
+    # oganisations_list = organisations_api_client.get_organisations
+
+    # Remplir la liste en dehors du IF <- reculer de 1 de pour l'indentation ;)
+    return render_template('views/platform-admin/usage_for_all_services_by_organisation.html', form=form, organisations=organisations_client.get_organisation)
 
 
 @main.route("/platform-admin/complaints")
