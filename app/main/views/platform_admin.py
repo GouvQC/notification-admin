6c9fc1ce6a2873fe9a72bf28d7c11ce15aaa8edb
Samuel Jacques <samuel.jacques.ext@retraitequebec.gouv.qc.ca>
2020-12-02 10:20:49 -0500

    Ajout des organisations dans la liste et recuperation des valeurs

--- app/main/views/platform_admin.py
+++ app/main/views/platform_admin.py
@@ -27,6 +27,7 @@ from app.main.forms import (
     PDFUploadForm,
     RequiredDateFilterForm,
     ReturnedLettersForm,
+    GetServicesByOrganisationForm,
 )
 from app.notify_client.api_key_api_client import api_key_api_client
 from app.statistics_utils import (
@@ -342,42 +343,38 @@ def usage_for_all_services():
 @main.route("/platform-admin/reports/usage-for-all-services-by-organisation", methods=['GET', 'POST'])
 @user_is_platform_admin
 def usage_for_all_services_by_organisation():
-    form = RequiredDateFilterForm()
-
+    form = GetServicesByOrganisationForm()
+    form.organisations.choices = [(org.id, org.name) for org in Organisations()]
+    
     if form.validate_on_submit():
-        # organisation_id = form.organisations.data
+        organisation_id = form.organisations.data
         start_date = form.start_date.data
         end_date = form.end_date.data
-        # headers = ["organisation_id", "organisation_name", "service_id", "service_name",
-        #            "sms_cost", "sms_fragments", "letter_cost", "letter_breakdown"]
 
-        # result = billing_api_client.get_usage_for_all_services_by_organisation(organisation_id, start_date, end_date)
-        # rows = [
-        #     [
-        #         r['organisation_id'], r["organisation_name"], r["service_id"], r["service_name"],
-        #         r["sms_cost"], r['sms_fragments'], r["letter_cost"], r["letter_breakdown"].strip()
-        #     ]
-        #     for r in result
-        # ]
-        # if rows:
-        #     return Spreadsheet.from_rows([headers] + rows).as_csv_data, 200, {
-        #         'Content-Type': 'text/csv; charset=utf-8',
-        #         'Content-Disposition': 'attachment; filename="Usage for all services from {} to {}.csv"'.format(
-        #             start_date, end_date
-        #         )
-        #     }
-        # else:
-        #     flash('No results for dates')
-        # flash('On a réussi à peser sur le bouton  ' + str(organisation_id) + ' ' + str(start_date) + ' ' + str(end_date))
+        headers = ["organisation_id", "organisation_name", "service_id", "service_name",
+                   "sms_cost", "sms_fragments", "letter_cost", "letter_breakdown"]
 
-        # organisations = organisations_client.get_organisations()
-        flash('On a réussi à peser sur le bouton  ' + str(start_date) + ' ' + str(end_date))
-        # flash('Voici ENFIN LE JSON 2 ' + type(organisations).__name__)
+        result = billing_api_client.get_usage_for_all_services_by_organisation(organisation_id, start_date, end_date)
+        rows = [
+            [
+                r['organisation_id'], r["organisation_name"], r["service_id"], r["service_name"],
+                r["sms_cost"], r['sms_fragments'], r["letter_cost"], r["letter_breakdown"].strip()
+            ]
+            for r in result
+        ]
+        if rows:
+            return Spreadsheet.from_rows([headers] + rows).as_csv_data, 200, {
+                'Content-Type': 'text/csv; charset=utf-8',
+                'Content-Disposition': 'attachment; filename="Usage for all services from {} to {}.csv"'.format(
+                    start_date, end_date
+                )
+            }
+        else:
+            flash('No results for dates')
 
     return render_template(
         'views/platform-admin/usage_for_all_services_by_organisation.html',
-        form=form,
-        organisations=Organisations()
+        form=form
     )
 
 
